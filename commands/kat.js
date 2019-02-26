121d9a5a5555e8edcaaaa7f855701b02d9ecff51
Gabriel Morris <gabemorris@outlook.com>
2019-02-25 17:43:37 -0800

    updated kat

--- commands/kat.js
+++ commands/kat.js
@@ -1,7 +1,12 @@
 exports.run = (client, message, args) => {
+  const { Attachment } = require('discord.js');
+
+  const meme = new Attachment('https://i.imgur.com/yTUeJcT.jpg');
+
   message.channel
     .send(
-      'ALL HAIL KAT, <@371151824331210755>\nKAT APPRECIATION DAY\nKAT APPRECIATION DAY\nKAT APPRECIATION DAY\nKAT APPRECIATION DAY'
+      'ALL HAIL KAT, <@371151824331210755>\nKAT APPRECIATION DAY\nKAT APPRECIATION DAY\nKAT APPRECIATION DAY\nKAT APPRECIATION DAY',
+      meme
     )
-    .catch(console.error);
+    .catch(err => console.error(err));
 };
