2be9480a360727d6e7573e0c7009dcaa54f7ffe8
Chris <christian.l.sedlacek@gmail.com>
2019-03-12 20:42:26 -0500

    Bugfix character new command erroring if no second/third arguments are provided

--- commands/character.js
+++ commands/character.js
@@ -52,11 +52,17 @@ exports.run = (client, message, args) => {
     // If new is an arg but not in the right spot don't do anything
     if (args[0] !== 'new') {
       return;
+    }
+    else if (!args[1]) {
+	    channel.send('Please specify a valid class name.');
     } else if (!classNames.find(name => name === args[1].toLowerCase())) {
       // If the class name isn't found send a message saying the class name is not valid
       channel.send('Invalid class name');
 
       return;
+    }
+    else if (!args[2]) {
+	    channel.send('Please specify a set of pronouns.');
     } else if (!pronouns.find(pronoun => pronoun === args[2].toLowerCase())) {
       // If the pronouns arent in the pronoun consts file send a message saying that
       channel.send('Invalid pronouns (male, female, or neutral)');
