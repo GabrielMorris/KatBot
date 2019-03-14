66f9fc974bd3a441f55411d6f5a31268772d56b0
Chris <christian.l.sedlacek@gmail.com>
2019-03-13 21:08:28 -0500

    Recreate calculateStats function in stats module

--- dragon-sword/characters/stats.js
+++ dragon-sword/characters/stats.js
@@ -1,5 +1,33 @@
 // utils
 const characterUtils = require('../../utils/character-utils');
+const classes = require('../../constants/character-classes');
+
+/**
+ * Creates an object representing a character's stats at a given level
+ * @private
+ * @param {Character} character Character model object to generate stats from
+ * @param {Object} levelObj Object with character level information
+ * @returns {Object} Object containing stat information
+ */
+function calculateStats(character, levelObj) {
+  // Get the character's class
+  const charClass = classes.find(
+    charClass => charClass.name === character.class
+  );
+  // Deconstruct the level number and base and growth stats for the class
+  const { level } = levelObj;
+  const { base, growth } = charClass;
+
+  // Return the stats modified for level
+  return {
+    HP: base.HP + growth.HP * level,
+    MP: base.MP + growth.MP * level,
+    STR: base.STR + growth.STR * level,
+    DEF: base.DEF + growth.DEF * level,
+    AGI: base.AGI + growth.AGI * level,
+    LUCK: base.LUCK + growth.LUCK * level
+  };
+}
 
 /**
  * Gets a character's stats as an object
@@ -7,7 +35,7 @@ const characterUtils = require('../../utils/character-utils');
  * @returns {Object}
  */
 function getCharacterStats(character) {
-	return characterUtils.calculateStats(
+	return calculateStats(
 	character,
 	characterUtils.getCharacterLevel(character)
 	);
