1e60b2f8e0e54eb9d26fdc420c57aff4c5a5a166
Chris <christian.l.sedlacek@gmail.com>
2019-03-13 21:43:07 -0500

    Refactor damage calculator to use own functions instead of combat-utils

--- dragon-sword/combat/damage-calculator.js
+++ dragon-sword/combat/damage-calculator.js
@@ -1,19 +1,45 @@
-// utils 
-const combatUtils = require('../../utils/combat-utils');
-
 const stats = require('../characters/stats');
-
+const rngUtils = require('../../utils/rng-utils');
 
 /**
- * Calculates damage caused by character attacking a monster
+ * Random chance (50/50) to return either 'positive' or 'negative'
+ * @returns {String} 'positive' or 'negative' randomly
+ */
+function damageType() {
+  return rngUtils.rollPercentage() < 0.5 ? 'positive' : 'negative';
+}
+
+/**
+ * Calculates a random damage-deviation amount based on given combined-stat value
+ * @param {Number} combinedStats Combined-stat value to use for damage-deviation calculation
+ * @returns {Number} Number representing random damage-deviation amount
+ */
+function randomDamage(combinedStats) {
+  return Math.ceil(rngUtils.rollPercentage() * (combinedStats * 0.1 * 0.2));
+}
+
+/**
+ * Calculates final damage value for given combined stat value
+ * @param {Number} combinedStats Combined-stat value to use for damage calculation
+ * @returns {Number} Number representing damage amount
+ */
+function calculateCharacterAttackDamage(attackerStats) {
+	const combinedAttackerStats = attackerStats.STR +attackerStats.AGI;
+	const damageRand = randomDamage(combinedAttackerStats);
+
+	return damageType() === 'positive'
+	? Math.ceil(combinedAttackerStats * 0.1) + damageRand
+	: Math.ceil(combinedAttackerStats * 0.1) - damageRand;
+}
+
+/**
+ * Rolls damage caused by character attacking a monster
  * @param {Character} attackingCharacter Character model object attacking monster
  * @returns {Number} Integer representing amount of damage attack would cause
  */
 function rollCharacterDamageMonster(attackingCharacter) {
 	const characterStats = stats.getCharacterStats(attackingCharacter);
-	const combinedStats = characterStats.STR +characterStats.AGI;
-
-	return combatUtils.attackDamage(combinedStats);
+	return calculateCharacterAttackDamage(characterStats);
 }
 
 module.exports = {
