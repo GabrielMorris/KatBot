47ef30545fc7cacc4b0682ed3310760506d611c2
Chris <christian.l.sedlacek@gmail.com>
2019-03-13 21:35:13 -0500

    Add rng util with percentage roll function

---
+++ utils/rng-utils.js
@@ -0,0 +1,13 @@
+const random = require('random');
+
+/**
+ * Rolls for a percentage between 0 and 100 (in decimal/float form)
+ * @returns {Number} Random float between 0 and 1
+ */
+function rollPercentage() {
+	return random.float(0, 1);
+}
+
+module.exports = {
+	rollPercentage
+};
