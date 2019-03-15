6aa8608431f4d66a62436741bf3f97afc527de7c
Chris <christian.l.sedlacek@gmail.com>
2019-03-15 18:00:41 -0500

    Add sub-command to debug-game to immediately spawn a monster

--- commands/debug-game.js
+++ commands/debug-game.js
@@ -1,3 +1,4 @@
+const { spawner } = require('../dragon-sword/spawner');
 const stateUtils = require('../utils/state-utils');
 
 exports.run = (client, message, args) => {
@@ -5,20 +6,30 @@ exports.run = (client, message, args) => {
   const gameMasters = stateUtils.getGameMasters();
 
   if (gameMasters.includes(message.author.id)) {
-    Game.findOne({ guildID: message.guild.id }).then(gameDoc => {
-      console.log(gameDoc);
+	Game.findOne({ guildID: message.guild.id }).then(gameDoc => {
+		console.log(gameDoc);
 
-      message.channel.send(
-        `Guild: ${gameDoc.guildID}\nAlive: ${gameDoc.monsterAlive}\nMonster:${
-          gameDoc.monster
-            ? `${gameDoc.monster.name}\nHealth: ${
-                gameDoc.monster.healthCurrent
-              }/${gameDoc.monster.health}\nXP value:${gameDoc.monster.xpValue}`
-            : null
-        }`
-      );
-    });
-  } else {
-    message.channel.send('You do not have permission to use this command.');
-  }
+		console.log('debug args:');
+		console.dir(args);
+		// Spawns a random monster in the channel
+		if (args.includes('monsterspawn')) {
+			console.log('running monster spawner');
+			spawner(message.channel);
+			return;
+		}
+
+		console.log('sending debug info');
+		message.channel.send(
+			`Guild: ${gameDoc.guildID}\nAlive: ${gameDoc.monsterAlive}\nMonster:${
+			gameDoc.monster
+			? `${gameDoc.monster.name}\nHealth: ${
+			gameDoc.monster.healthCurrent
+			}/${gameDoc.monster.health}\nXP value:${gameDoc.monster.xpValue}`
+			: null
+			}`
+		);
+	});
+	} else {
+		message.channel.send('You do not have permission to use this command.');
+	}
 };
