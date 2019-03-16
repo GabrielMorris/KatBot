ccccd5d720f4cc8712dc4182501f3d85dc3caa7a
Gabriel Morris <gabemorris@outlook.com>
2019-03-15 22:13:56 -0700

    top is now paginated to stay within the 1024 char field limit

--- commands/top.js
+++ commands/top.js
@@ -5,10 +5,24 @@ exports.run = (client, message, args) => {
 
   Level.find({ guildID })
     .sort({ experience: -1 })
-    .limit(30)
     .then(levelDocs => {
-      const topArray = levelDocs.map(
-        (doc, index) => `${index + 1}. <@${doc.memberID}> - ${doc.experience}xp`
+      let docs;
+
+      if (!args[0]) {
+        docs = levelDocs.slice(0, 10);
+      } else if (args[0] === '2') {
+        docs = levelDocs.slice(10, 20);
+      } else if (args[0] === '3') {
+        docs = levelDocs.slice(20, 30);
+      } else {
+        docs = levelDocs.slice(0, 10);
+      }
+
+      const topArray = docs.map(
+        doc =>
+          `${_getIndex(doc, levelDocs)}. <@${doc.memberID}> - ${
+            doc.experience
+          }xp`
       );
 
       const embedOpts = {
@@ -27,3 +41,16 @@ exports.run = (client, message, args) => {
     })
     .catch(err => console.error(err));
 };
+
+function _getIndex(doc, mongoDocs) {
+  // const found = mongoDocs.find(mDoc => mDoc.memberID === doc.memberID);
+  let index;
+
+  mongoDocs.forEach((item, i) => {
+    if (item.memberID === doc.memberID) {
+      index = i;
+    }
+  });
+
+  return index + 1;
+}
