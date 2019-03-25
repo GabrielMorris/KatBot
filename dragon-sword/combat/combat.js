79ba1878f660bfb87a123fb6199d52af6f6ae4e3
Gabriel Morris <gabemorris@outlook.com>
2019-03-25 10:35:34 -0700

    HP restored on level up

--- dragon-sword/combat/combat.js
+++ dragon-sword/combat/combat.js
@@ -12,13 +12,16 @@ const characterUtils = require('../../utils/character-utils');
  * @returns {Object} Object containing updated combat state
  */
 function executeCharacterAttackPhase(combatState) {
-	const phaseInfo = {
-		attack: null
-	};
-	// Attack monster
-	phaseInfo.attack = combatAttacks.characterAttackMonster(combatState.character, combatState.monster);
-	combatState.phases.characterAttackPhase = phaseInfo;
-	return combatState;
+  const phaseInfo = {
+    attack: null
+  };
+  // Attack monster
+  phaseInfo.attack = combatAttacks.characterAttackMonster(
+    combatState.character,
+    combatState.monster
+  );
+  combatState.phases.characterAttackPhase = phaseInfo;
+  return combatState;
 }
 
 /**
@@ -28,28 +31,30 @@ function executeCharacterAttackPhase(combatState) {
  * @returns {Object} Object containing updated combat state
  */
 function executeMonsterAttackPhase(combatState) {
-	const phaseInfo = {
-		attack: null
-	};
+  const phaseInfo = {
+    attack: null
+  };
 
-	// exit phase immediately if monster is dead
-	if (combatChecks.checkMonsterDead(combatState.monster)) {
-		return combatState;
-	}
+  // exit phase immediately if monster is dead
+  if (combatChecks.checkMonsterDead(combatState.monster)) {
+    return combatState;
+  }
 
-	// monster attack check
-	const monsterRetaliates = combatChecks.checkMonsterRetaliates(combatState.monster);
+  // monster attack check
+  const monsterRetaliates = combatChecks.checkMonsterRetaliates(
+    combatState.monster
+  );
 
-	// monster attack succeeds
-	if (monsterRetaliates) {
-		phaseInfo.attack = combatAttacks.monsterAttackCharacter(
-			combatState.monster,
-			combatState.character
-		);
-	}
+  // monster attack succeeds
+  if (monsterRetaliates) {
+    phaseInfo.attack = combatAttacks.monsterAttackCharacter(
+      combatState.monster,
+      combatState.character
+    );
+  }
 
-	combatState.phases.monsterAttackPhase = phaseInfo;
-	return combatState;
+  combatState.phases.monsterAttackPhase = phaseInfo;
+  return combatState;
 }
 
 /**
@@ -59,45 +64,60 @@ function executeMonsterAttackPhase(combatState) {
  * @returns {Object} Modified combat state
  */
 function executeRewardStep(combatState) {
-	const rewardInfo = {
-		gold: null,
-		experience: null
-	};
+  const rewardInfo = {
+    gold: null,
+    experience: null
+  };
 
-	const rewardCharacter = combatState.character;
-	const rewardMonster = combatState.monster;
+  const rewardCharacter = combatState.character;
+  const rewardMonster = combatState.monster;
 
-	// only grant rewards if monster is dead
-	if (combatChecks.checkMonsterDead(combatState.monster)) {
-		// Get the character's current level
-		const oldLevel = levels.getCharacterLevel(rewardCharacter);
+  // only grant rewards if monster is dead
+  if (combatChecks.checkMonsterDead(combatState.monster)) {
+    // Get the character's current level
+    const oldLevel = levels.getCharacterLevel(rewardCharacter);
 
-		// Grant rewards post-combat
-		const combatRewardsEarned = rewards.rewardCharacterCombat(rewardCharacter, rewardMonster);
-		rewardInfo.gold = combatRewardsEarned.gold;
-		rewardInfo.experience = combatRewardsEarned.experience;
+    // Grant rewards post-combat
+    const combatRewardsEarned = rewards.rewardCharacterCombat(
+      rewardCharacter,
+      rewardMonster
+    );
 
-		// Get the level again
-		const newLevel = levels.getCharacterLevel(rewardCharacter);
+    rewardInfo.gold = combatRewardsEarned.gold;
+    rewardInfo.experience = combatRewardsEarned.experience;
 
-		// If the levels are different, then they've leveled up
-		if (oldLevel.level !== newLevel.level) {
-			// Get the old/new stats object and level up the character
-			const stats = characterUtils.handleLevelUp(
-				rewardCharacter,
-				oldLevel,
-				newLevel
-			);
-			combatState.levelUp = {
-				oldLevel,
-				newLevel,
-				statDiff: stats
-			};
-		}
-	}
+    // Get the level again
+    const newLevel = levels.getCharacterLevel(rewardCharacter);
 
-	combatState.rewards = rewardInfo;
-	return combatState;
+    // If the levels are different, then they've leveled up
+    if (oldLevel.level !== newLevel.level) {
+      // Get the old/new stats object and level up the character
+      const stats = characterUtils.handleLevelUp(
+        rewardCharacter,
+        oldLevel,
+        newLevel
+      );
+
+      combatState.levelUp = {
+        oldLevel,
+        newLevel,
+        statDiff: stats
+      };
+
+      // Get the new base stats
+      const baseStats = characterUtils.calculateStats(
+        combatState.character,
+        newLevel
+      );
+
+      // Set characters HP to max
+      combatState.character.health = baseStats.HP;
+    }
+  }
+
+  combatState.rewards = rewardInfo;
+
+  return combatState;
 }
 
 /**
@@ -107,11 +127,11 @@ function executeRewardStep(combatState) {
  * @returns {Object} Modified combat state
  */
 function executeCombatStep(combatState) {
-	executeCharacterAttackPhase(combatState);
-	executeMonsterAttackPhase(combatState);
-	return combatState;
-}
+  executeCharacterAttackPhase(combatState);
+  executeMonsterAttackPhase(combatState);
 
+  return combatState;
+}
 
 /**
  * Executes combat state save to datastore
@@ -120,17 +140,19 @@ function executeCombatStep(combatState) {
  * @returns {Promise.<Object>} Promise resolving to modified combat state
  */
 function executeSaveStep(combatState) {
-	combatState.game.markModified('monster');
+  combatState.game.markModified('monster');
 
-	const saveMonsterAlive = !(combatChecks.checkMonsterDead(combatState.monster));
+  const saveMonsterAlive = !combatChecks.checkMonsterDead(combatState.monster);
 
-	return stateUtils.setGameState(combatState.game, saveMonsterAlive, combatState.monster)
-	.then(() => {
-		return combatState.character.save();
-	}).then(() => {
-		combatState.saved = true;
-		return combatState;
-	});
+  return stateUtils
+    .setGameState(combatState.game, saveMonsterAlive, combatState.monster)
+    .then(() => {
+      return combatState.character.save();
+    })
+    .then(() => {
+      combatState.saved = true;
+      return combatState;
+    });
 }
 
 /**
@@ -139,12 +161,14 @@ function executeSaveStep(combatState) {
  * @returns {Promise.<Object>} Promise resolving to modified combat state
  */
 function executeCombat(combatState) {
-	let combatStateUpdated;
-	combatStateUpdated = executeCombatStep(combatState);
-	combatStateUpdated = executeRewardStep(combatState);
-	return executeSaveStep(combatState);
+  let combatStateUpdated;
+
+  combatStateUpdated = executeCombatStep(combatState);
+  combatStateUpdated = executeRewardStep(combatState);
+
+  return executeSaveStep(combatState);
 }
 
 module.exports = {
-	executeCombat
+  executeCombat
 };
