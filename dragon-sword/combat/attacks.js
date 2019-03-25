89acf24bffacb182375fd0f780b79595d6d2bca4
Gabriel Morris <gabemorris@outlook.com>
2019-03-25 10:12:10 -0700

    Fix #52

--- dragon-sword/combat/attacks.js
+++ dragon-sword/combat/attacks.js
@@ -8,23 +8,27 @@ const accuracyCalculator = require('./accuracy-calculator');
  * @returns {{hit: Boolean, damageRoll: Number, rawDamageRoll: Number}} Object containing information about the attack
  */
 function characterAttackMonster(attackingCharacter, targetMonster) {
-    const hitsEnemy = accuracyCalculator.rollCharacterHitMonster(attackingCharacter);
-    const characterDamageRoll = damageCalculator.rollCharacterDamageMonster(attackingCharacter);
-    // Don't let damage take a monster into negative HP
+  const hitsEnemy = accuracyCalculator.rollCharacterHitMonster(
+    attackingCharacter
+  );
+  const characterDamageRoll = damageCalculator.rollCharacterDamageMonster(
+    attackingCharacter
+  );
+  // Don't let damage take a monster into negative HP
   const cappedDamageRoll =
     targetMonster.healthCurrent - characterDamageRoll < 0
       ? targetMonster.healthCurrent
       : characterDamageRoll;
 
-    if (hitsEnemy) {
-	targetMonster.healthCurrent -= cappedDamageRoll;
-    }
+  if (hitsEnemy) {
+    targetMonster.healthCurrent -= cappedDamageRoll;
+  }
 
-    return {
-	    hit: hitsEnemy,
-	    damageRoll: cappedDamageRoll,
-	    rawDamageRoll: characterDamageRoll
-    }
+  return {
+    hit: hitsEnemy,
+    damageRoll: cappedDamageRoll,
+    rawDamageRoll: characterDamageRoll
+  };
 }
 
 /**
@@ -34,7 +38,9 @@ function characterAttackMonster(attackingCharacter, targetMonster) {
  * @returns {{damageRoll: Number, rawDamageRoll: Number}} Object containing information about the attack
  */
 function monsterAttackCharacter(attackingMonster, targetCharacter) {
-  const monsterDamageRoll = damageCalculator.rollMonsterDamageCharacter(attackingMonster);
+  const monsterDamageRoll = damageCalculator.rollMonsterDamageCharacter(
+    attackingMonster
+  );
   // Don't let damage take a character into negative HP
   const cappedMonsterDamage =
     targetCharacter.health - monsterDamageRoll < 0
@@ -44,13 +50,13 @@ function monsterAttackCharacter(attackingMonster, targetCharacter) {
   // Damage character
   targetCharacter.health -= cappedMonsterDamage;
 
-	return {
-		damageRoll: cappedMonsterDamage,
-		rawDamageRoll: monsterDamageRoll
-	}
+  return {
+    damageRoll: cappedMonsterDamage,
+    rawDamageRoll: monsterDamageRoll
+  };
 }
 
 module.exports = {
-	characterAttackMonster,
-	monsterAttackCharacter
+  characterAttackMonster,
+  monsterAttackCharacter
 };
