ecefda44372eae000e658c2804a490d13225d0f8
Gabriel Morris <gabemorris@outlook.com>
2019-10-15 13:12:00 -0600

    changed bg colors

--- constants/embeds.js
+++ constants/embeds.js
@@ -1,8 +1,8 @@
 const embeds = {
   color: 'af2e1a',
   images: {
-    top: 'https://i.imgur.com/mKvNabC.png',
-    rank: 'https://i.imgur.com/p1Vhj2h.png',
+    top: 'https://i.imgur.com/cGCcYmh.png',
+    rank: 'https://i.imgur.com/kFVpn2F.png',
     help: 'https://i.imgur.com/msJqq6O.png',
     memes: 'https://i.imgur.com/Tc1jSlx.png',
     rest: 'https://i.imgur.com/hIaTgoy.png'
