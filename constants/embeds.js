5f02d244dba901bf959947ee103a6f7b24cb929a
Gabriel Morris <gabemorris@outlook.com>
2019-10-15 13:22:53 -0600

    ugh

--- constants/embeds.js
+++ constants/embeds.js
@@ -1,8 +1,8 @@
 const embeds = {
   color: 'af2e1a',
   images: {
-    top: 'https://i.imgur.com/qeG3a4s.png',
-    rank: 'https://i.imgur.com/kFVpn2F.png',
+    top: 'https://i.imgur.com/Gpzkb6n.png',
+    rank: 'https://i.imgur.com/d0usp4V.png',
     help: 'https://i.imgur.com/msJqq6O.png',
     memes: 'https://i.imgur.com/Tc1jSlx.png',
     rest: 'https://i.imgur.com/hIaTgoy.png'
