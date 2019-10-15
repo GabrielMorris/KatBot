9380b77d876a1822ef46a2893cf8a013887147a7
Gabriel Morris <gabemorris@outlook.com>
2019-10-15 13:25:01 -0600

    UGH

--- constants/embeds.js
+++ constants/embeds.js
@@ -1,7 +1,7 @@
 const embeds = {
   color: 'af2e1a',
   images: {
-    top: 'https://i.imgur.com/Gpzkb6n.png',
+    top: 'https://i.imgur.com/BvAonuy.png',
     rank: 'https://i.imgur.com/d0usp4V.png',
     help: 'https://i.imgur.com/msJqq6O.png',
     memes: 'https://i.imgur.com/Tc1jSlx.png',
